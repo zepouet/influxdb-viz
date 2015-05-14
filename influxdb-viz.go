252ecf4adb77c7334859ed64db2cc795bfd1cea6
Nicolas <zepouet@gmail.com>
2015-05-14 19:38:48 +0200

    Add new route

--- influxdb-viz.go
+++ influxdb-viz.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"net/http"
 	"github.com/gin-gonic/gin"
 	"html/template"
 	//"github.com/spf13/viper"
@@ -20,7 +21,7 @@ const (
 func main() {
 	router := gin.Default()
 
-	// create static route for all files in this folder
+	// create routes for all static files
 	router.Static("/images", STATIC_IMAGES)
 	router.Static("/js", STATIC_JS)
 	router.Static("/css", STATIC_CSS)
@@ -29,14 +30,14 @@ func main() {
 	router.GET("/", func(c *gin.Context) {
 		obj := gin.H{"title": "Index"}
 		router.SetHTMLTemplate(template.Must(template.ParseFiles(TEMPLATE_MAIN, TEMPLATE_INDEX)))
-		c.HTML(200, "base", obj)
+		c.HTML(http.StatusOK, "base", obj)
 	})
 
 	// bubbles page
 	router.GET("/bubbles", func(c *gin.Context) {
-		obj := gin.H{"title": "Index"}
+		obj := gin.H{"title": "Bubbles"}
 		router.SetHTMLTemplate(template.Must(template.ParseFiles(TEMPLATE_MAIN, TEMPLATE_BUBBLES)))
-		c.HTML(200, "base", obj)
+		c.HTML(http.StatusOK, "base", obj)
 	})
 
 	router.Run(":8080")
