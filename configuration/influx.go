94b311aedd2be84cbda710104d58e5f635f0cf9a
Nicolas <zepouet@gmail.com>
2015-06-20 22:32:47 +0200

    Add header licence + viper configuration through file.toml

--- configuration/influx.go
+++ configuration/influx.go
@@ -1,3 +1,25 @@
+// The MIT License (MIT)
+//
+// Copyright (c) 2015 Nicolas MULLER (@zepouet)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 package configuration
 
 import (
@@ -5,7 +27,8 @@ import (
 	"log"
 	"fmt"
 	"github.com/influxdb/influxdb/client"
-	//"github.com/spf13/viper"
+	"github.com/spf13/viper"
+	"runtime"
 )
 
 type InfluxConfig struct {
@@ -17,19 +40,31 @@ type InfluxConfig struct {
 
 func (Config *InfluxConfig) Init() {
 
+	// The configuration file will be named config.toml
 	viper.SetConfigName("config")
-	viper.AddConfigPath("/etc/init.d/influxdb-viz/")   // path to look for the config file in
-	viper.AddConfigPath("$HOME/.influxdb-viz")  // call multiple times to add many search paths
+	viper.SetConfigType("toml")
+	// We could search the configuration file into these directories
+	viper.AddConfigPath("$HOME/.influxdb-viz")
+	viper.AddConfigPath("$HOME/settings")
+
 	err := viper.ReadInConfig() // Find and read the config file
 	if err != nil { // Handle errors reading the config file
-		panic(fmt.Errorf("Fatal error config file: %s \n", err))
+		fmt.Println("No configuration file loaded - using defaults")
+		fmt.Println("Fatal error config file: %s \n", err)
 	}
 
-	Config.InfluxDbHost = "localhost"
-	Config.InfluxDbPort = 8086
-	Config.InfluxDbPassword = "root"
-	Config.InfluxDbUser = "root"
+	log.Println("InfluxDbHost " + viper.GetString("InfluxDbHost"))
+	log.Println("InfluxDbPort " + viper.GetString("InfluxDbPort"))
+	log.Println("InfluxDbUser " + viper.GetString("InfluxDbUser"))
+	log.Println("InfluxDbPassword " + viper.GetString("InfluxDbPassword"))
 
+	Config.InfluxDbHost = viper.GetString("InfluxDbHost")
+	Config.InfluxDbPort = viper.GetInt("InfluxDbPort")
+	Config.InfluxDbUser = viper.GetString("InfluxDbUser")
+	Config.InfluxDbPassword = viper.GetString("InfluxDbPassword")
+
+	// Halt and catch fire
+	runtime.GOMAXPROCS(runtime.NumCPU())
 }
 
 func (config *InfluxConfig) Verify() {
@@ -48,5 +83,6 @@ func (config *InfluxConfig) Verify() {
 	}
 	log.Printf("Happy to be connected to InfluxDB! %v, %s", dur, ver)
 }
+
 
 
--- influxdb-viz.go
+++ influxdb-viz.go
@@ -1,3 +1,25 @@
+// The MIT License (MIT)
+//
+// Copyright (c) 2015 Nicolas MULLER (@zepouet)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 package main
 
 import (
@@ -17,6 +39,12 @@ const (
 
 func main() {
 
+	// read the configuration and exits if necessary
+	influxConfig := &configuration.InfluxConfig{}
+	influxConfig.Init()
+	influxConfig.Verify()
+
+	// initialise the web engine
 	router := gin.Default()
 
 	// create routes for all static files
@@ -25,10 +53,6 @@ func main() {
 	router.Static("/css", STATIC_CSS)
 	router.Static("/json", STATIC_JSON)
 	router.Static("/tags", STATIC_TAGS)
-
-	influxConfig := &configuration.InfluxConfig{}
-	influxConfig.Init()
-	influxConfig.Verify()
 
 	// add route for index page
 	homepageController := &controllers.HomepageController{}
